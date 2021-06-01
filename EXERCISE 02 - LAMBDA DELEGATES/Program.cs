06d554f1188a13a34d9feac3634d25360b63be8d
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-01 17:45:14 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - LAMBDA DELEGATES/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Services;
+namespace EXERCISE_02___LAMBDA_DELEGATES {
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program {
+        static void Main(string[] args) {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+             
+        }
+    }
+}
